ca6d0f4424380208d04a7b07b5fb06df1f808f71
Bhavish295 <bhavishkumar799@gmail.com>
2026-08-08 21:58:28 +0500

    Update project

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -2,7 +2,10 @@ import nodemailer from "nodemailer";
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "softnovatech.pk@gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
